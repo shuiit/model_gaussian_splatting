4c2a97449ba130f792a88b0bf2cbb623899cffb8
roni-maya <roni.maya@mail.huji.ac.il>
2025-03-12 20:14:38 +0200

    LT

--- Utils.py
+++ Utils.py
@@ -29,39 +29,6 @@ def qvec2rotmat(qvec):
          2 * qvec[2] * qvec[3] + 2 * qvec[0] * qvec[1],
          1 - 2 * qvec[1]**2 - 2 * qvec[2]**2]])
 
-def get_dict_for_points3d(frames):
-    """
-    Create dictionaries mapping 3D voxel positions and their mean colors.
-
-    This function processes voxel data from multiple frames to generate a dictionary
-    that associates unique voxel identifiers with their 3D positions and another dictionary 
-    that maps voxel identifiers to their corresponding mean color values.
-
-    Args:
-        frames (dict): A dictionary containing frame data, where each key is an image identifier 
-                       and each value has attributes for voxel indices and pixel colors.
-
-    Returns:
-        tuple: A tuple containing:
-            - dict: A dictionary mapping voxel identifiers to their 3D positions (list of floats).
-            - dict: A dictionary mapping voxel identifiers to their mean color values (list of floats).
-    """
-
-    # generates the dictionary of all 3d-2d mappings from every frame. 
-    colors = np.hstack([frames[im_name].color_of_pixel for im_name in frames.keys()])
-    all_voxels = np.column_stack((np.vstack(([(frames[im_name].voxels_with_idx) for im_name in frames.keys()])),colors))
-    unique_voxels  = np.unique(all_voxels[:,0:4],axis = 0)
-
-    voxel_dict = {vxl[3]:list(vxl[0:3]) for vxl in unique_voxels}
-    [voxel_dict[vxl[3]].extend(vxl[4:-1]) for vxl in all_voxels]
-
-    # calculate mean color
-    colors_dict = {vxl[3]:[] for vxl in unique_voxels}
-    for vxl in all_voxels:
-        colors_dict[vxl[3]].extend(vxl[6:])
-    colors_dict = {key: [np.mean(np.array(values)).astype(int)]*3 for key, values in colors_dict.items()}
-    return voxel_dict,colors_dict
-
 
 
 def triangulate_least_square(origins,end_of_vectors):
@@ -89,18 +56,6 @@ def triangulate_least_square(origins,end_of_vectors):
     c = np.sum([np.dot(mat,vec) for mat,vec in zip(inner,origins)],axis = 0)
     return  np.linalg.solve(s,c)
 
-def load_hull(body_wing,path):
-    """
-    Load the 3D hull points for a specified body part from a .mat file.
-
-    Args:
-        body_wing (str): The name of the body part ('body', 'rwing', or 'lwing').
-        path (str): The directory path where the .mat file is located.
-
-    Returns:
-        numpy.ndarray: An array containing the 3D hull points for the specified body part.
-    """
-    return scipy.io.loadmat(f'{path}/3d_pts/{body_wing}.mat')['hull']
 
 
 def intersection_per_cam(frames_per_cam, cam_num, ptcloud_volume, tol=1.0):
