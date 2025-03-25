a5280fa791606b93deb1ec9402a57d91fbc78e74
RONI-PC\Roni <roniamit1@gmail.com>
2025-03-25 11:34:10 +0200

    PC

--- model/model_utils.py
+++ model/model_utils.py
@@ -23,9 +23,9 @@ def initilize_skeleton_and_skin(path_to_mesh,skeleton_scale = 1,skin_scale = 1):
     list_joints_pitch_update = [neck,right_sp_no_bone,left_sp_no_bone]
 
 
-    body = Skin(f'{path_to_mesh}/body.stl',scale = skin_scale,color = 'lime')
-    right_wing = Skin(f'{path_to_mesh}/right_wing.stl',scale = skin_scale,constant_weight = right_wing_root,color = 'crimson')
-    left_wing = Skin(f'{path_to_mesh}/left_wing.stl',scale = skin_scale, constant_weight = left_wing_root,color = 'dodgerblue')
+    body = Skin(f'{path_to_mesh}/body_remesh.stl',scale = skin_scale,color = 'lime')
+    right_wing = Skin(f'{path_to_mesh}/right_wing_lowres.stl',scale = skin_scale,constant_weight = right_wing_root,color = 'crimson')
+    left_wing = Skin(f'{path_to_mesh}/left_wing_lowres.stl',scale = skin_scale, constant_weight = left_wing_root,color = 'dodgerblue')
 
     return root,body,right_wing,left_wing,list_joints_pitch_update
 
